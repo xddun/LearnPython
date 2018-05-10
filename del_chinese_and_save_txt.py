1c485bd480d6d4a84ab09eab48fb5f3a989ee128
xd <137712826@qq.com>
2018-05-10 17:57:47 +0800

    Create del_chinese_and_save_txt.py

---
+++ del_chinese_and_save_txt.py
@@ -0,0 +1,17 @@
+# -!- coding=utf-8 -!-
+import re
+import sys
+
+f = open('q.txt', 'r', encoding='UTF-8')
+
+txts = ''
+txts = f.read()
+
+m = re.findall(r'[^\u4e00-\u9fa5]', txts)
+
+strresult = ''
+fq = open('result.txt', 'w', encoding='UTF-8')
+strresult = strresult.join(m)
+
+fq.write(strresult)
+ 
